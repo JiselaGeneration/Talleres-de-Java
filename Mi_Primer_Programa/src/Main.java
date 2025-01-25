07ed2d1db22d1525f98f9fc732a5b8a4bdc36235
jiselageneration <jisela.castillo@genstudents.org>
2025-01-25 14:20:55 -0500

    Dos nuevas carpetas

---
+++ Mi_Primer_Programa/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String test = "Hello";
+        test += " World!";
+        test += " Welcome to Generation";
+
+        // Imprime el contenido de la variable test
+        System.out.println(test);
+    }
+}
