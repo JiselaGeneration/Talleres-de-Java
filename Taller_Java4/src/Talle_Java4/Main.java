31567ee992c4bc562b74c2b8a176addf31ed6050
jiselageneration <jisela.castillo@genstudents.org>
2025-01-25 14:07:36 -0500

    Eliminé el directorio nombre_del_directorio

--- Taller_Java4/src/Talle_Java4/Main.java
+++
@@ -1,9 +0,0 @@
-package Talle_Java4;
-
-public class Main {
-    public static void main(String[] args) {
-        IndiceMasaCorporal.indiceMasaCorporal();
-        ComparadorDeEdad.comparadorDeEdad();
-        ParticipacionEvento.participacionEvento();
-    }
-}
